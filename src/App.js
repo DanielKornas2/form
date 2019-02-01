2a94529ed83cf03b094c2b2e6fce5306af1bac6b
Daniel Kornas <harvi_pl@wp.pl>
2019-02-01 15:36:29 +0100

    From structure, initial state, handle events and basic styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    accept: false,
+  }
+
+  handleChange = (e) => {
+    // name = odczytuję name z inputa, np. email i na tej podstawie ustawiam stan
+    const name = e.target.name;
+    const type = e.target.type;
+
+    if (type === "checkbox") {
+      const isChecked = e.target.checked;
+
+      // uzywam nawiasow kwadratowych bo przekazuje zmienna jako parametr
+      this.setState({
+       [name]: isChecked,
+      })
+    } else {
+      this.setState({
+        [name]: e.target.value,
+      })
+    }
+    
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit= {this.handleSubmit} noValidate>
+          <label htmlFor="user">Imię:
+          <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="email">Email:
+          <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="password">Hasło:
+          <input type="password" id="password" name="password" value={this.state.password} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="accept">
+          <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange} /> Akceptuję regulamin.
+          </label>
+
+          <button>Wyślij</button>
+        </form>
       </div>
     );
   }
 }
 
 export default App;
+ 
