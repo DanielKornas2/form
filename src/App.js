a225fb7c0d78c02910d4b887b45400c77fef7c94
Daniel Kornas <harvi_pl@wp.pl>
2019-02-01 16:08:00 +0100

    Erros, conditional rendering, css

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ class App extends Component {
     email: "",
     password: "",
     accept: false,
+
+    errors: {
+      username: false,
+      email: false,
+      password: false,
+      accept: true,
+    }
+  }
+
+  warnings = {
+    username_incorrect: "Nazwa nie może zawierać spacji i musi być dłuższa niż 2 litery",
+    email_incorrect: "Email musi zawierać @",
+    password_incorrect: "Hasło musi być dłuższe niż 5 znaków",
+    accept_incorrect: "Zakceptuj regulamin.",
   }
 
   handleChange = (e) => {
@@ -41,19 +55,29 @@ class App extends Component {
         <form onSubmit= {this.handleSubmit} noValidate>
           <label htmlFor="user">Imię:
           <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange} />
+          {/* && - skrócony zapis ifa ? , oznacza, ze to co jest za && wykona sie tylko kiedy jest true.
+          tzn jezeli jest error w state (true), to pokaze komunikat o bledzie */}
+          {this.state.errors.username && <span>
+          {this.warnings.username_incorrect}</span>}
           </label>
 
           <label htmlFor="email">Email:
           <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange} />
+          {this.state.errors.email && <span>
+          {this.warnings.email_incorrect}</span>}
           </label>
 
           <label htmlFor="password">Hasło:
           <input type="password" id="password" name="password" value={this.state.password} onChange={this.handleChange} />
+          {this.state.errors.password && <span>
+          {this.warnings.password_incorrect}</span>}
           </label>
 
           <label htmlFor="accept">
           <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange} /> Akceptuję regulamin.
           </label>
+          {this.state.errors.accept && <span>
+          {this.warnings.accept_incorrect}</span>}
 
           <button>Wyślij</button>
         </form>
